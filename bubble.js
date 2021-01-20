891d55c969664fe86a5b533aa1c176820e57b451
cyrillkuettel <36520284+cyrillkuettel@users.noreply.github.com>
2021-01-20 22:20:38 +0100

    Add files via upload

--- bubble.js
+++ bubble.js
@@ -1,63 +1,53 @@
 function Bubble(x, y) {
   this.x = x;
   this.y = y;
+  this.diameter = 200;
+  this.mittelpunktX = this.x + this.diameter / 2
+  this.mittelpunktY = this.y + this.diameter / 2
   this.col = color(255, 204, 0);
-  this.diameter = 100;
+
   //boolean to check if there is a note in this object
   this.written = false;
   this.content = content;
 
+  this.setPosition = function(x, y) { //oben links
+    this.x = x;
+    this.y = y;
+  }
   this.isWritten = function() {
     return this.written;
   }
 
   this.display = function() {
-
     fill(this.col);
     rect(this.x, this.y, this.diameter, this.diameter);
     if (this.written) {
-      textSize(13)
-      // Text wraps within text box
+      textSize(13) // Text wraps within text box
       fill(0)
       text(this.content, this.x, this.y, this.diameter, this.diameter)
     }
-
   }
 
   this.clicked = function() {
-    var d = dist(mouseX, mouseY, this.x, this.y);
+    // this method better for a circle, but it's close enough for the moment
+    var d = dist(mouseX, mouseY, this.mittelpunktX, this.mittelpunktY);
     if (d < this.diameter / 2) {
+      console.log(d);
       this.col = color(15, 79, 120);
     }
-  };
+  }
 
-  this.move = function() {
-    //this.x = this.x + random(-1, 1);
-    //this.y = this.y + random(-1, 1);
-    function f(x) {
-      //  y = ax + b
-      return 5.3 * x + 4
-    }
-
-    this.x += 1;
-    this.y = f(x)
-
-  };
-
-  // enter note with enter key: if enter is pressed and textfield not empthy: new Bubble objects
   this.write = function(content) {
-    //   this function just updates the content
     if (!(content == "")) {
-      // check if content is Nothing
       this.content = content;
       this.written = true;
     }
   }
+
   this.changeSize = function(d) {
     this.diameter = d;
   }
   this.changeColor = function(c) {
-
     this.col = c
   }
 
